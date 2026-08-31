7c3850794ac694c8851de2c3b96de79077ffe6fb
unknown <abiyyurahid20@gmail.com>
2026-08-31 16:34:21 +0700

    perf(image): set unoptimized images to bypass worker image resizing and serve static assets directly

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next';
 import withBundleAnalyzer from '@next/bundle-analyzer';
 
 const nextConfig: NextConfig = {
+  images: {
+    unoptimized: true,
+  },
   turbopack: {},
   webpack: (config, { isServer, webpack }) => {
     if (isServer) {
